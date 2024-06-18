233880d468e852fabcf59a5a432f794eb015dfcb
Belendia Serda <belendia@gmail.com>
2024-06-18 10:10:20 +0300

    Change the handler function to get and add comment

--- json.go
+++ json.go
@@ -10,9 +10,16 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
 		log.Printf("Responding with 5XX error: %s", msg)
 	}
+	// json:"error" - This directive instructs the json.Marshal function to format the error as specified below.
+	// `
+	// 	{
+	// 		"error": "xxxxxxx"
+	// 	}
+	// `
 	type errorResponse struct {
 		Error string `json:"error"`
 	}
+
 	respondWithJSON(w, code, errorResponse{
 		Error: msg,
 	})
