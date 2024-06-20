76f1381cf55e743beb55b034be28f9d31c54356c
Belendia Serda <belendia@gmail.com>
2024-06-20 10:11:41 +0300

    Add postgresql database and create users endpoint

--- go.mod
+++ go.mod
@@ -2,9 +2,11 @@ module github.com/belendia/rssagg
 
 go 1.22.4
 
-require github.com/joho/godotenv v1.5.1
-
 require (
-	github.com/go-chi/chi/v5 v5.0.12
+	github.com/go-chi/chi/v5 v5.0.13
 	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/belendia/rssagg/internal/database"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
@@ -15,6 +23,22 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT environment variable is not set")
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect with database.", err)
+	}
+
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
 	}
 
 	router := chi.NewRouter()
@@ -28,6 +52,7 @@ func main() {
 	}))
 
 	v1Router := chi.NewRouter()
+	v1Router.Post("/users", apiCfg.handlerUsersCreate)
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 
